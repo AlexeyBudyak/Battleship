02646212775b26a5c4b2363907434da9d2bf8d7c
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-07-28 22:58:56 -0700

    Add files via upload

---
+++ quotes_learner/intro.py
@@ -0,0 +1,17 @@
+
+def intro():
+    print ("""
+      Q
+    Q u o t e s
+      o       
+      t   Ⓠ Ⓛ  
+    L e a r n e r
+      s
+    📖     =      ⏳       ⚙️
+"Learning is a Lifelong Process"
+ l......g is a l......g p.....s
+    
+     Peter Drucker
+    pet🐇R  Dr. Ucker 
+--------------------------------
+""")
